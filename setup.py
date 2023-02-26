46ee8c1608d984862fa05ff9d6e3cd5f82e8594b
xiashanfengjian <79048696+xiashanfengjian@users.noreply.github.com>
2023-02-26 21:55:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'requests', 'numpy', 'matplotlib', 'pandas', 'akshare', 'dateutil'
+    'requests', 'numpy', 'matplotlib', 'pandas', 'akshare'
 ]
 
 # What packages are optional?
